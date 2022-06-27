2dffccae33e9df317c8823e66538f46f97f3852c
noahthayne <noahthayne26@gmail.com>
2022-06-26 19:27:04 -0600

    2 6/26/2022

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementsByClassName('menu-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+menuButton.addEventListener('click', {
+    navbarLinksclassList.toggle('active')
+})
