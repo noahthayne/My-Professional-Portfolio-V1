88538fdb0d4407bb4953e9e55405b034176b9209
noah.thayne <noahthayne26@gmail.com>
2022-06-27 22:36:28 -0600

    javascript corrections

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuButton = document.getElementsByClassName('menu-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-menuButton.addEventListener('click', {
-    navbarLinksclassList.toggle('active')
+menuButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
 })
